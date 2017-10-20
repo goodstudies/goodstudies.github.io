b29546f8a71c494864f4b481ec924efb15f21ba9
Ramanan Sambandam <ramanan.java@gmail.com>
2017-10-20 19:35:52 +0530

    currency conversion app first commit

---
+++ PWACurrencyConverter/js/app.js
@@ -0,0 +1,43 @@
+  var selection = 0; //0=From Currency, 1=ToCurrency
+
+  var app = {
+  	from: "USD",
+  	to: "INR",
+  	fromCurrencyName: "US Dollar",
+  	toCurrencyName: "Indian Rupee",
+  	currencies: {},
+  	currencyCodes: ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"],
+  	currncyNames: ["Australian Dollar", "Bulgarian Lev", "Brazilian Real", "Canadian Dollar", "Swiss Franc", "Chinese Yuan", "Czech Koruna", "Danish Krone", "Euro", "British Pound", "Hong Kong Dollar", "Croatian Kuna", "Hungarian Forint", "Indonesian Rupiah", "Israeli Shekel", "Indian Rupee", "Japanese Yen", "Korean Won", "Mexican Peso", "Malaysian Ringgit", "Norwegian Krone", "New Zealand Dollar", "Philippine Peso", "Polish Zloty", "Romanian Leu", "Russian Rouble", "Swedish Krona", "Singapore Dollar", "Thai Baht", "Turkey Lira", "US Dollar", "South African Rand"]
+  };
+
+  function getCurrencies() {
+    var url = 'http://api.fixer.io/latest?base=USD';
+
+    // Fetch the latest data.
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          app.currencies = JSON.parse(request.response);
+          console.log(JSON.stringify(app.currencies.rates));
+        }
+      } 
+    };
+    request.open('GET', url);
+    request.send();
+  };
+
+  function onSelect(currency, type) {
+  	if(type == 0) {
+  		document.getElementById('fromCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+  	} else {
+  		document.getElementById('toCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+  	}
+
+  	document.getElementById('result').innerHTML = app.currencies.rates[currency];
+  }
+
+  document.getElementById('fromCurrency').innerHTML = app.fromCurrencyName;
+  document.getElementById('toCurrency').innerHTML = app.toCurrencyName;
+
+  getCurrencies();
