46d91ec02a0a7ff633096559f2460b6c24061a5e
Ramanan Sambandam <ramanan.java@gmail.com>
2017-10-23 15:29:59 +0530

    service worker added

--- PWACurrencyConverter/js/app.js
+++ PWACurrencyConverter/js/app.js
@@ -9,35 +9,51 @@
   	currencyCodes: ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"],  	currncyNames: ["Australian Dollar", "Bulgarian Lev", "Brazilian Real", "Canadian Dollar", "Swiss Franc", "Chinese Yuan", "Czech Koruna", "Danish Krone", "Euro", "British Pound", "Hong Kong Dollar", "Croatian Kuna", "Hungarian Forint", "Indonesian Rupiah", "Israeli Shekel", "Indian Rupee", "Japanese Yen", "Korean Won", "Mexican Peso", "Malaysian Ringgit", "Norwegian Krone", "New Zealand Dollar", "Philippine Peso", "Polish Zloty", "Romanian Leu", "Russian Rouble", "Swedish Krona", "Singapore Dollar", "Thai Baht", "Turkey Lira", "US Dollar", "South African Rand"]
   };
 
-  function getCurrencies() {
-    var url = 'https://api.fixer.io/latest?base=USD';
+  var url = 'https://api.fixer.io/latest?base=USD';
 
-    // Fetch the latest data.
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-          app.currencies = JSON.parse(request.response);
-          updateCurrency();
-          
-        }
-      } 
-    };
-    request.open('GET', url);
-    request.send();
+function getCurrencies() {
+  // Fetch the latest data.
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        app.currencies = JSON.parse(request.response);
+        updateCurrency();
+        saveCurrencyRates();
+      } else {
+        loadPreviousCurrencyRates();
+      }
+    } else {
+      loadPreviousCurrencyRates();
+    }
   };
+  request.open('GET', url);
+  request.send();
+};
 
-  function onSelect(currency, type) {
-  	if(type == 0) {
-      document.getElementById('fromCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
-      app.from = currency;
-  	} else {
-      document.getElementById('toCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
-      app.to = currency;
-  	}
+function saveCurrencyRates() {
+  var currencies = JSON.stringify(app.currencies);
+  localStorage.currencies = currencies;
+}
 
-  	updateCurrency();
+function loadPreviousCurrencyRates() {
+  app.currencies = localStorage.currencies;
+  if (app.currencies) {
+    app.currencies = JSON.parse(app.currencies);
   }
+}
+
+function onSelect(currency, type) {
+  if(type == 0) {
+    document.getElementById('fromCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+    app.from = currency;
+  } else {
+    document.getElementById('toCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+    app.to = currency;
+  }
+
+  updateCurrency();
+}
 
 function updateCurrency() {
   var from = (app.from != "USD") ? app.currencies.rates[app.from] : 1;
@@ -60,3 +76,9 @@ app.from.innerHTML = app.fromCurrencyName;
 app.to.innerHTML = app.toCurrencyName;
 
 getCurrencies();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('./service-worker.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
