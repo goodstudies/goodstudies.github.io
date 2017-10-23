587d3d7355ed6b58961619aef5f2b8d875fbd522
Ramanan Sambandam <ramanan.java@gmail.com>
2017-10-23 13:58:20 +0530

    all currency conversion logic implemented

--- PWACurrencyConverter/js/app.js
+++ PWACurrencyConverter/js/app.js
@@ -6,8 +6,7 @@
   	fromCurrencyName: "US Dollar",
   	toCurrencyName: "Indian Rupee",
   	currencies: {},
-  	currencyCodes: ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"],
-  	currncyNames: ["Australian Dollar", "Bulgarian Lev", "Brazilian Real", "Canadian Dollar", "Swiss Franc", "Chinese Yuan", "Czech Koruna", "Danish Krone", "Euro", "British Pound", "Hong Kong Dollar", "Croatian Kuna", "Hungarian Forint", "Indonesian Rupiah", "Israeli Shekel", "Indian Rupee", "Japanese Yen", "Korean Won", "Mexican Peso", "Malaysian Ringgit", "Norwegian Krone", "New Zealand Dollar", "Philippine Peso", "Polish Zloty", "Romanian Leu", "Russian Rouble", "Swedish Krona", "Singapore Dollar", "Thai Baht", "Turkey Lira", "US Dollar", "South African Rand"]
+  	currencyCodes: ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"],  	currncyNames: ["Australian Dollar", "Bulgarian Lev", "Brazilian Real", "Canadian Dollar", "Swiss Franc", "Chinese Yuan", "Czech Koruna", "Danish Krone", "Euro", "British Pound", "Hong Kong Dollar", "Croatian Kuna", "Hungarian Forint", "Indonesian Rupiah", "Israeli Shekel", "Indian Rupee", "Japanese Yen", "Korean Won", "Mexican Peso", "Malaysian Ringgit", "Norwegian Krone", "New Zealand Dollar", "Philippine Peso", "Polish Zloty", "Romanian Leu", "Russian Rouble", "Swedish Krona", "Singapore Dollar", "Thai Baht", "Turkey Lira", "US Dollar", "South African Rand"]
   };
 
   function getCurrencies() {
@@ -19,7 +18,8 @@
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           app.currencies = JSON.parse(request.response);
-          console.log(JSON.stringify(app.currencies.rates));
+          updateCurrency();
+          
         }
       } 
     };
@@ -29,15 +29,34 @@
 
   function onSelect(currency, type) {
   	if(type == 0) {
-  		document.getElementById('fromCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+      document.getElementById('fromCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+      app.from = currency;
   	} else {
-  		document.getElementById('toCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+      document.getElementById('toCurrency').innerHTML = app.currncyNames[app.currencyCodes.indexOf(currency)];
+      app.to = currency;
   	}
 
-  	document.getElementById('result').innerHTML = app.currencies.rates[currency];
+  	updateCurrency();
   }
 
-  document.getElementById('fromCurrency').innerHTML = app.fromCurrencyName;
-  document.getElementById('toCurrency').innerHTML = app.toCurrencyName;
+function updateCurrency() {
+  var from = (app.from != "USD") ? app.currencies.rates[app.from] : 1;
+  var to = (app.to != "USD") ? app.currencies.rates[app.to] : 1;
 
-  getCurrencies();
+  var ratio = (1/from) * 100;
+
+  var valueToConvert = document.getElementById('amount').value;
+
+  if(valueToConvert <= 0) {
+    valueToConvert = 1;
+  }
+
+  var calculated = ((to * ratio) / 100) * valueToConvert;
+
+  document.getElementById('result').innerHTML = (calculated).toFixed(2);
+}
+
+app.from.innerHTML = app.fromCurrencyName;
+app.to.innerHTML = app.toCurrencyName;
+
+getCurrencies();
